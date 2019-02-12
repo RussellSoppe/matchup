238a12738bdc2516b618f429542a1a5e8c5a36ae
russellicious <42648750+russellicious@users.noreply.github.com>
2019-02-12 13:15:06 -0600

    adding default sizes to pics

--- themes/birds/birds.js
+++ themes/birds/birds.js
@@ -1,21 +1,21 @@
 export const birdsthemearray = [
-	`<img src="themes/birds/bluejay.png"/>`, 
-	`<img src="themes/birds/cardinal.png"/>`, 
-	`<img src="themes/birds/chicadee.png"/>`, 
-	`<img src="themes/birds/eagle.png"/>`,
+	`<img src="themes/birds/bluejay.png" height="100%" width="auto"/>`, 
+	`<img src="themes/birds/cardinal.png" height="100%" width="auto"/>`, 
+	`<img src="themes/birds/chicadee.png" height="100%" width="auto"/>`, 
+	`<img src="themes/birds/eagle.png" height="100%" width="auto"/>`,
 
-	`<img src="themes/birds/hawk.png"/>`, 
-	`<img src="themes/birds/hummer.png"/>`,
-	`<img src="themes/birds/oriole.png"/>`,
-	`<img src="themes/birds/parrot.png"/>`,
+	`<img src="themes/birds/hawk.png" height="100%" width="auto"/>`, 
+	`<img src="themes/birds/hummer.png" height="100%" width="auto"/>`,
+	`<img src="themes/birds/oriole.png" height="100%" width="auto"/>`,
+	`<img src="themes/birds/parrot.png" height="100%" width="auto"/>`,
 
-	`<img src="themes/birds/angryblues.png" height="70px" width="auto"/>`, 
-	`<img src="themes/birds/angrybomb.png" height="70px" width="auto"/>`,
-	`<img src="themes/birds/angrychuck.png" height="70px" width="auto"/>`,
-	`<img src="themes/birds/angryred.png" height="70px" width="auto"/>`,
+	`<img src="themes/birds/angryblues.png" height="100%" width="auto"/>`, 
+	`<img src="themes/birds/angrybomb.png" height="100%" width="auto"/>`,
+	`<img src="themes/birds/angrychuck.png" height="100%" width="auto"/>`,
+	`<img src="themes/birds/angryred.png" height="100%" width="auto"/>`,
 
-	`<img src="themes/birds/paintedtoucan.png" height="70px" width="auto"/>`,
-	`<img src="themes/birds/waterhouse.png" height="70px" width="auto"/>`,
-	`<img src="themes/birds/waterflying.png" height="70px" width="auto"/>`,
-	`<img src="themes/birds/watersparrow.png" height="70px" width="auto"/>`
+	`<img src="themes/birds/paintedtoucan.png" height="100%" width="auto"/>`,
+	`<img src="themes/birds/waterhouse.png" height="100%" width="auto"/>`,
+	`<img src="themes/birds/waterflying.png" height="100%" width="auto"/>`,
+	`<img src="themes/birds/watersparrow.png" height="100%" width="auto"/>`
 ];
--- themes/shapes/shapes.js
+++ themes/shapes/shapes.js
@@ -1,21 +1,21 @@
 export const shapesthemearray = [
-`<img src="themes/shapes/circle.png"/>`,
-`<img src="themes/shapes/triangle.png"/>`,
-`<img src="themes/shapes/cross.png"/>`,
-`<img src="themes/shapes/octagon.png"/>`,
+`<img src="themes/shapes/circle.png" height="100%" width="auto"/>`,
+`<img src="themes/shapes/triangle.png" height="100%" width="auto"/>`,
+`<img src="themes/shapes/cross.png" height="100%" width="auto"/>`,
+`<img src="themes/shapes/octagon.png" height="100%" width="auto"/>`,
 
-`<img src="themes/shapes/heart.png"/>`,
-`<img src="themes/shapes/cube.png"/>`,
-`<img src="themes/shapes/arrow.png"/>`,
-`<img src="themes/shapes/diamond.png"/>`,
+`<img src="themes/shapes/heart.png" height="100%" width="auto"/>`,
+`<img src="themes/shapes/cube.png" height="100%" width="auto"/>`,
+`<img src="themes/shapes/arrow.png" height="100%" width="auto"/>`,
+`<img src="themes/shapes/diamond.png" height="100%" width="auto"/>`,
 
-`<img src="themes/shapes/cylinder.png"/>`,
-`<img src="themes/shapes/heptagon.png"/>`,
-`<img src="themes/shapes/oval.png"/>`,
-`<img src="themes/shapes/pentagon.png"/>`,
+`<img src="themes/shapes/cylinder.png" height="100%" width="auto"/>`,
+`<img src="themes/shapes/heptagon.png" height="100%" width="auto"/>`,
+`<img src="themes/shapes/oval.png" height="100%" width="auto"/>`,
+`<img src="themes/shapes/pentagon.png" height="100%" width="auto"/>`,
 
-`<img src="themes/shapes/rectangle.png"/>`,
-`<img src="themes/shapes/star.png"/>`,
-`<img src="themes/shapes/trapezoid.png"/>`,
-`<img src="themes/shapes/square.png"/>`
+`<img src="themes/shapes/rectangle.png" height="100%" width="auto"/>`,
+`<img src="themes/shapes/star.png" height="100%" width="auto"/>`,
+`<img src="themes/shapes/trapezoid.png" height="100%" width="auto"/>`,
+`<img src="themes/shapes/square.png" height="100%" width="auto"/>`
 ];
